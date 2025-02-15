8d928dc3e4de150da19533269b54ebbe9b451d5f
maheepisher <55838286+maheepisher@users.noreply.github.com>
2025-02-14 23:41:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,5 +28,5 @@ const gracefulShutdown = () => {
 };
 
 process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown);
 process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
